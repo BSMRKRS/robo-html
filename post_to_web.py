a818e78341c3a916933e1268c99af5fc44b4da60
avoss19 <avoss19@bsmschool.org>
2018-02-02 15:57:13 -0800

    one function instead of 2

--- post_to_web.py
+++ post_to_web.py
@@ -2,28 +2,12 @@
 ################################
 
 #Begin with incude post_to_web as PTW
-#put PTW.post() somewhere in your loop
-#update the PTW.state dictionary with your data
+#put PTW.send(x) somewhere in your loop
 #visit 192.168.21.x to view your dashboard
-
-import time
-
-global tState
-tState = time.time()
-global state
-state = dict()
-#print 'ptw', state, tState
 
 f = open('readings.txt','r+')
 
-def send(d):
+def send(text):
   f.seek(0)
-  msg = (',').join(map(lambda reading: str(reading) + '=' + str(d[reading]), d.keys()))
-  f.write('let msg = `' + msg+'`;')
+  f.write('let msg = `' + str(text)+'`;')
   f.truncate()
-
-def post(interval = 0.5):
-  global tState
-  if time.time() - tState > interval:
-    send(state)
-    tState = time.time()
